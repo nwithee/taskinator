8acf90c624adad72be5ba9103ad56f6ca6818c29
nzwithee@gmail.com <Cartoon#01>
2022-02-23 14:27:06 -0600

    a few ui tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,13 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form, broh");
+        return false;
+    }
+
+    formEl.reset();
+
     //package the captured data into an object
     var taskDataObj = {
         name: taskNameInput,
